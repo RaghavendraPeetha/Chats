ee5b3da0c62e7baece44247adb8142584d198f4e
RaghavendraPeetha <87773958+RaghavendraPeetha@users.noreply.github.com>
2024-07-21 19:07:53 +0530

    Update config.js
    changed URI_KEY

--- chat-backend/config.js
+++ chat-backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_URI: 'mongodb+srv://ragavap1001:Rmt3RYMElvRFgvRL@cluster0.six4zvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    MONGO_URI: 'mongodb+srv://{URI_KEY}.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     JWT_SECRET: 'chats-app-jwt-secret'
   };
   
